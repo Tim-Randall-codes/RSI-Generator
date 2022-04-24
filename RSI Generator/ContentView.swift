49e5c110b1fe9fb3b2ba875df1db3549bc811124
Tim Randall <trtimrandall@gmail.com>
2022-04-24 20:50:54 +1000

    day one

--- RSI Generator/ContentView.swift
+++ RSI Generator/ContentView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import SpriteKit
 
-struct ContentView: View {
+struct GameView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 390, height: 844)
+        scene.scaleMode = .fill
+        return scene
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SpriteView(scene: scene)
+                    .ignoresSafeArea()
+    }
+}
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        run(SKAction.repeatForever(
+              SKAction.sequence([
+                SKAction.run(addCircle),
+                SKAction.wait(forDuration: 0.2)
+                ])))
+    }
+    func addCircle() {
+        let circle = SKShapeNode(circleOfRadius: 50)
+        circle.fillColor = .yellow
+        let randomX = Int.random(in: 1..<391)
+        circle.position = CGPoint(x: randomX, y: 844)
+        addChild(circle)
+        
+        let actionMove = SKAction.move(to: CGPoint(x: circle.position.x, y: 0), duration: TimeInterval(3.0))
+        let actionMoveDone = SKAction.removeFromParent()
+        
+        circle.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
 }
--- RSI Generator/RSI_GeneratorApp.swift
+++ RSI Generator/RSI_GeneratorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RSI_GeneratorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GameView()
         }
     }
 }
