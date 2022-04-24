90d907a9c6d85e90e0d552446c9e242b6a893ba7
Tim Randall <trtimrandall@gmail.com>
2022-04-24 19:37:36 +1000

    first commit

---
+++ RSI Generator/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  RSI Generator
+//
+//  Created by Tim Randall on 24/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ RSI Generator/RSI_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSI_GeneratorApp.swift
+//  RSI Generator
+//
+//  Created by Tim Randall on 24/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RSI_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
